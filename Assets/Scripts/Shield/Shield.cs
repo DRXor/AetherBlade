148149869c57f2a77c933cfd8852652803ca135d
Жанна Островская <ostrovskaazanna8@gmail.com>
2025-11-23 20:06:52 +0300

    Changed the file structure

--- Assets/Scripts/Shield/Shield.cs
+++ Assets/Scripts/Shield/Shield.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+п»їusing UnityEngine;
 using UnityEngine.Events;
 
 public class Shield : MonoBehaviour
@@ -6,8 +6,8 @@ public class Shield : MonoBehaviour
     [Header("Shield Settings")]
     public float maxShield = 50f;
     public float currentShield;
-    public bool hasShieldItem = false; 
-    public bool isShieldActive = false; 
+    public bool hasShieldItem = false;
+    public bool isShieldActive = false;
 
     [Header("Events")]
     public UnityEvent OnShieldDamage;
@@ -23,6 +23,26 @@ public class Shield : MonoBehaviour
         isShieldActive = false;
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleShield();
+        }
+    }
+
+    void ToggleShield()
+    {
+        if (isShieldActive)
+        {
+            DeactivateShield();
+        }
+        else
+        {
+            ActivateShield();
+        }
+    }
+
     public bool TakeDamage(float damage)
     {
         if (!isShieldActive || currentShield <= 0)
@@ -31,15 +51,12 @@ public class Shield : MonoBehaviour
         currentShield -= damage;
         OnShieldDamage?.Invoke();
 
-        Debug.Log($"{gameObject.name} took {damage} shield damage. Shield: {currentShield}");
-
         if (currentShield <= 0)
         {
             currentShield = 0;
             isShieldActive = false;
-            hasShieldItem = false; 
+            hasShieldItem = false;
             OnShieldBreak?.Invoke();
-            Debug.Log($"{gameObject.name}'s shield broke permanently!");
         }
 
         return true;
@@ -50,8 +67,6 @@ public class Shield : MonoBehaviour
         hasShieldItem = true;
         currentShield = shieldAmount;
         OnShieldPickup?.Invoke();
-
-        Debug.Log($"Щит подобран! Прочность: {currentShield}/{maxShield}");
     }
 
     public void ActivateShield()
@@ -60,15 +75,6 @@ public class Shield : MonoBehaviour
         {
             isShieldActive = true;
             OnShieldActivate?.Invoke();
-            Debug.Log($"Щит активирован! Прочность: {currentShield}/{maxShield}");
-        }
-        else if (!hasShieldItem)
-        {
-            Debug.Log("Нет щита в инвентаре!");
-        }
-        else if (isShieldActive)
-        {
-            Debug.Log("Щит уже активирован!");
         }
     }
 
@@ -78,17 +84,11 @@ public class Shield : MonoBehaviour
         {
             isShieldActive = false;
             OnShieldDeactivate?.Invoke();
-            Debug.Log("Щит деактивирован");
         }
     }
 
     public float GetShieldPercentage()
     {
         return currentShield / maxShield;
-    }
-
-    public void LogStatus()
-    {
-        Debug.Log($"Статус: В инвентаре={hasShieldItem}, Активен={isShieldActive}, Прочность={currentShield}/{maxShield}");
     }
 }
