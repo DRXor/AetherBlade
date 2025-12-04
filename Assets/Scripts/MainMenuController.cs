5e99692c8e949a0708305d604b979ab6015518d9
Janna <ostrovskaazanna8@gmail.com>
2025-12-04 21:22:58 +0300

    Chore: restored editor quit logic and updated level background

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -41,6 +41,11 @@ public class MainMenuController : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("Выход из игры...");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 
     void Update()
