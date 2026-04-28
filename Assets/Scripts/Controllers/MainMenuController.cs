72c06161469892e1742bf017ae6824d46e4792ca
Janna <ostrovskaazanna8@gmail.com>
2026-04-28 23:44:32 +0300

    Adjust random order of scenes

--- Assets/Scripts/Controllers/MainMenuController.cs
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
@@ -8,16 +7,8 @@ public class MainMenuController : MonoBehaviour
     public Button startButton;
     public Button quitButton;
 
-    [Header("Название сцены с игрой")]
-    public string gameSceneName = "FirstAct";
-
     void Start()
     {
-        if (startButton == null)
-            Debug.LogError("Start Button не назначена!");
-        if (quitButton == null)
-            Debug.LogError("Quit Button не назначена!");
-
         if (startButton != null)
             startButton.onClick.AddListener(StartGame);
 
@@ -27,15 +18,16 @@ public class MainMenuController : MonoBehaviour
 
     public void StartGame()
     {
-        Debug.Log("Загружаем игру...");
+        Debug.Log("Генерируем случайные уровни и запускаем игру...");
 
-        if (SceneUtility.GetBuildIndexByScenePath(gameSceneName) < 0)
+        if (GameManager.Instance != null)
         {
-            Debug.LogError($"Сцена '{gameSceneName}' не найдена! Добавь её в Build Settings.");
-            return;
+            GameManager.Instance.StartRandomGame();
         }
-
-        SceneManager.LoadScene(gameSceneName);
+        else
+        {
+            Debug.LogError("GameManager не найден на сцене!");
+        }
     }
 
     public void QuitGame()
